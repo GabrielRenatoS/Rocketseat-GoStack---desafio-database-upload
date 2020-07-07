2a4867388c77aa5ca4c767710b0e1a1e7435d6bb
GabrielRenatoS <grenatosilva@gmail.com>
2020-07-07 17:12:10 -0300

    Corrections to Delete and Repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,8 +11,6 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // const transactionsRepository = getRepository(Transaction);
-
     const transactions = await this.find();
 
     const { income, outcome } = transactions.reduce(
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -8,7 +8,7 @@ class DeleteTransactionService {
   public async execute(id: string): Promise<void> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
 
-    const transaction = await transactionsRepository.findOne({ where: id });
+    const transaction = await transactionsRepository.findOne(id);
 
     if (!transaction) {
       throw new AppError('No transaction found');
